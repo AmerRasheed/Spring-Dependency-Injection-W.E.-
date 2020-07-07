9be30e69ac5e935162f5ca84a53529f3361db5d0
AmerRasheed <59695079+AmerRasheed@users.noreply.github.com>
2020-07-08 01:25:11 +0200

    Initial Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }
+
+// I want to use InJECT AppUserSequencerImpl INSIDE AppUserINMemoryStorage
+
+// Dependancy Injection CAN ONLY BE PERFORMED INSIDE THE BEANS
+
+
+//Now after XML AppUserIdSequencerImpl BECOME BEAN
+
+// Now AppUserInMemoryStorage NEEDS ALSO TO BECOME BEAN IN ORDER TO
--- src/main/java/org/example/data/AppUserInMemoryStorage.java
+++ src/main/java/org/example/data/AppUserInMemoryStorage.java
@@ -1,6 +1,7 @@
 package org.example.data;
 
 import org.example.model.AppUser;
+import org.example.service.AppUserIdSequencer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +10,15 @@ public class AppUserInMemoryStorage implements AppUserDAO{
 
     private List<AppUser> users = new ArrayList<>();
 
+    private AppUserIdSequencer appUserIdSequencer;
+
+    public AppUserInMemoryStorage(AppUserIdSequencer appUserIdSequencer) {
+        this.appUserIdSequencer = appUserIdSequencer;
+    }
+
     @Override
     public AppUser createNewAppUser(String email, String password, String name) {
-    AppUser user = new AppUser(0,email,password,name);
+    AppUser user = new AppUser(appUserIdSequencer.nextid(),email,password,name);
 
     if(!users.contains(user)){
         users.add(user);
